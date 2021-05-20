622169a5382938caa8fdbc9e24377d6c26e3004f
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2021-05-20 15:12:33 +0900

    change window size

--- src/ship_frame.cpp
+++ src/ship_frame.cpp
@@ -13,9 +13,9 @@ namespace shipsim
 {
 
   ShipFrame::ShipFrame(rclcpp::Node::SharedPtr &node_handle, QWidget *parent, Qt::WindowFlags f)
-      : QFrame(parent, f), path_image_(500, 500, QImage::Format_ARGB32), path_painter_(&path_image_), frame_count_(0), id_counter_(0)
+      : QFrame(parent, f), path_image_(1200, 900, QImage::Format_ARGB32), path_painter_(&path_image_), frame_count_(0), id_counter_(0)
   {
-    setFixedSize(500, 500);
+    setFixedSize(1200, 900);
     setWindowTitle("ShipSim");
 
     srand(time(NULL));
